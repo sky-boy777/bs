4109f883d02158e18ae902c76fc8a4155ac1aa86
sky-boy777 <1251779123@qq.com>
2020-12-03 00:33:36 +0800

    第七次提交

--- my_project/apps/user_app/views.py
+++ my_project/apps/user_app/views.py
@@ -8,7 +8,7 @@ from .models import UserModel
 from werkzeug.security import generate_password_hash  # 密码加密
 from exts import db
 from utils.send_email import send_mail  # 发送邮件
-from itsdangerous import TimedJSONWebSignatureSerializer  # token
+from itsdangerous import TimedJSONWebSignatureSerializer, SignatureExpired  # token
 
 
 # 用户模块蓝图
@@ -17,20 +17,17 @@ user_bp = Blueprint(name='user', import_name=__name__, url_prefix='/user')
 
 @user_bp.route('/get_image_code')
 def get_image_code():
-    '''图片验证码链接'''
+    '''获取图片验证码'''
     # 生成图片验证码
     code, image_code = create_image_code()
-
     # 验证码存入session
     session['code'] = code
-
     # 创建缓冲区
     buffer = io.BytesIO()
     # 验证码图片存入缓冲区
     image_code.save(buffer, 'jpeg')
     # 获取缓冲区里二进制验证码图片
     b_img = buffer.getvalue()
-
     # 创建响应对象，响应头改为图片类型
     res = make_response(b_img)
     res.headers['Content-Type'] = 'image/jpg'
@@ -40,6 +37,7 @@ def get_image_code():
 @user_bp.route('/register', methods=['GET', 'POST'])
 def register():
     '''用户注册'''
+    # 实例化一个表单验证对象
     form = UserRegisterForm()
     if request.method == 'POST':
         # 表单验证
@@ -48,40 +46,77 @@ def register():
             email = request.form.get('email')  # POST:form，GET:args
             password = form.password.data
 
-            # 查询邮箱是否被注册，布尔值
-            is_register = UserModel.query.filter(UserModel.email == email).first()
-            if is_register:
-                return render_template('user/register.html', form=form, msg='邮箱已被注册')
+            # 如果用户已注册，且已激活，first()得到一个对象，不加得到查询集
+            # 方法一，取下标
+            # user = UserModel.query.filter(UserModel.email == email)
+            # user = user[0]
+            # 方法二，first()
+            user = UserModel.query.filter(UserModel.email == email).first()
 
-            # 注册
+            # 查询用户是否已存在数据库
+            if user:
+                # 用户存在且已激活
+                if user.is_activate == 1:
+                    return render_template('user/register.html', form=form, msg='邮箱已被注册')
+                elif user.is_delete == 0:
+                    # 用户存在但未激活，未销号（恶意账户被管理员逻辑删除），只更新用户密码，然后重新发送激活邮件
+                    user.password = generate_password_hash(password, salt_length=9)  # 密码加密
+                    try:
+                        db.session.commit()
+                    except:
+                        # 提交数据库出错
+                        return render_template('user/register.html', form=form, msg='注册失败')
+                    # 发邮件
+                    send_mail(email)
+                    return redirect(url_for(endpoint='user.login'))
+
+            # 新用户注册
             user = UserModel()
             user.email = email
             user.password = generate_password_hash(password, salt_length=9)  # 密码加密
             try:
-                pass
-                # db.session.add(user)
-                # db.session.commit()
+                db.session.add(user)
+                db.session.commit()
             except:
-                return render_template('user/register.html', form=form, msg='未知错误')
+                return render_template('user/register.html', form=form, msg='注册失败')
+            # 调用异步发送邮件函数，并重定向到登录页面
+            send_mail(email)
+            return redirect(url_for(endpoint='user.login'))
 
-            # 如果用户添加成功，则发送激活邮件
-            if user:
-                # 生成token，24小时后过期，secret_key加密
-                serializer = TimedJSONWebSignatureSerializer(secret_key=settings.SECRET_KEY, expires_in=86400)
-                token = serializer.dumps({email: email}).decode('utf-8')
-
-                # 发送激活邮件
-                result = send_mail(email, token)
-                # 发送成功
-                if result:
-                    return redirect(url_for(endpoint='user.login'))
-                # 发送失败
-                else:
-                    return render_template('user/register.html', form=form, msg='邮件发送失败')
         # 表单验证未通过
         return render_template('user/register.html', form=form)
     # get请求
     return render_template('user/register.html', form=form)
+
+
+@user_bp.route('/activate', methods=['GET'])
+def activate():
+    '''账户激活'''
+    print('**************************************开始')
+    # try:
+    try:
+        token = request.args.get('token')
+        serializer = TimedJSONWebSignatureSerializer(secret_key=settings.SECRET_KEY)
+        info = serializer.loads(token)
+        email = info.get('email')
+        print(token,'*******', email, '********************')
+    except SignatureExpired as e:
+        print('-------------------------------->>>>>>>>>>>>>>>>>>>>>>>>>链接过期')
+        return '激活链接过期，请重新注册'
+    try:
+        # 查找用户
+        user = UserModel.query.filter(UserModel.email == email).first()
+    except:
+        return '用户不存在'
+    if user:
+        user.is_activate = 1
+        db.session.commit()
+        return '激活成功，可以登录了'
+    else:
+        return '用户不存在'
+    # except:
+    #     # 未知错误重定向到主页
+    #     return redirect(url_for(endpoint='main.index'))
 
 
 @user_bp.route('/login', methods=['GET', 'POST'])
@@ -89,6 +124,9 @@ def login():
     '''用户登录'''
 
     return render_template('user/login.html')
+
+
+
 
 
 
--- my_project/utils/send_email.py
+++ my_project/utils/send_email.py
@@ -1,37 +1,35 @@
 from flask_mail import Message
 from exts import mail
 import settings
+import threading
+from itsdangerous import TimedJSONWebSignatureSerializer  # token
 
 
-def send_mail(email=None, token=None):
+def async_send_mail(msg):
+    '''异步发送邮件'''
+    # 在使用到的时候再导入app，在头部导入会出错
+    from app import app
+    # 创建应用上下文
+    with app.app_context():
+        mail.send(message=msg)
+
+
+def send_mail(email=None):
     '''发送邮件'''
-    msg = Message()
-    msg.subject = '旅游景点自助系统账户激活'  # 主题
-    msg.recipients = [email]  # 收件人
-    msg.html = '''<!DOCTYPE html>
-                <html lang="en">
-                <head>
-                    <meta charset="UTF-8">
-                    <title>账户激活</title>
-                </head>
-                <body>
-                <h1>欢迎注册</h1>
-                点击下面链接激活账号(24小时后过期)<br>
-                <a href="%s/user/active/?token=%s">%s/user/active/?token=%s</a>
-                </body>
-                </html>''' % (settings.LOCALHOST, token, settings.LOCALHOST, token)
-    # msg.html = '<h1>欢迎注册</h1> ' \
-    #            '点击下面链接激活账号(24小时后过期)<br>' \
-    #            '<a href="http://%s/user/active/?token=%s">%s/user/active/?token=%s</a>' \
-    #            % (settings.LOCALHOST, token, settings.LOCALHOST, token)
+    # 生成token，24小时后过期，secret_key加密
+    serializer = TimedJSONWebSignatureSerializer(secret_key=settings.SECRET_KEY, expires_in=120)
+    token = serializer.dumps({'email': email}).decode('utf-8')
 
-    # 发送
-    try:
-        mail.send(msg)
-        return True
-    except:
-        return False
+    # 创建邮件对象
+    html = '''<h1>欢迎注册</h1>点击下面链接激活账号(24小时后过期)<br>
+                    <a href="http://%s/user/activate?token=%s">%s/user/active/?token=%s</a>
+                    ''' % (settings.LOCALHOST, token, settings.LOCALHOST, token)
+    msg = Message(subject='旅游景点自助系统账户激活', recipients=[email], html=html)
+
+    # 创建一个线程，并启动
+    t = threading.Thread(target=async_send_mail, args=(msg,))
+    t.start()
 
 
 if __name__ == '__main__':
-    pass
+    print('pass')
